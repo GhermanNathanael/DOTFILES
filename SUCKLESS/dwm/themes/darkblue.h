d5d9b1f3e440e0b06906836c6b72f19ec09b26eb
GhermanNathanael <g.nathanael@tutanota.com>
2022-06-28 17:47:31 +0300

    My configs

---
+++ SUCKLESS/dwm/themes/darkblue.h
@@ -0,0 +1,10 @@
+/* Background color */
+static const char col_gray1[]       = "#222222";
+/* Inactive window border color */
+static const char col_gray2[]       = "#444444";
+/* Font color */ 
+static const char col_gray3[]       = "#bbbbbb";
+/* Current tag and current window font color */
+static const char col_gray4[]       = "#eeeeee";
+/* Top Bar second color and active window color */
+static const char col_cyan[]        = "#1500ff";
