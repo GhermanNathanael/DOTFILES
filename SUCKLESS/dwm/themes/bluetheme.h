7eec009dfe528515abd72e2d4d244e35e524b71e
gnathanael <104849108+GhermanNathanael@users.noreply.github.com>
2023-12-29 18:45:01 +0100

    Delete SUCKLESS directory

--- SUCKLESS/dwm/themes/bluetheme.h
+++
@@ -1,6 +0,0 @@
-static char normbgcolor[]           = "#000000";
-static char normbordercolor[]       = "#444444";
-static char normfgcolor[]           = "#bbbbbb";
-static char selfgcolor[]            = "#eeeeee";
-static char selbordercolor[]        = "#1515e1";
-static char selbgcolor[]            = "#1515e1"; // #0000FF #1515e1 #3a3ae9 #1500ff
